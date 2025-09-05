51027d4e7dc63572dbdbb6f0a89bc610f788eb40
tomeeagle <tomeagleweb@gmail.com>
2025-09-05 15:31:56 +0100

    initial commit

---
+++ assets/show-more.js
@@ -0,0 +1,25 @@
+if (!customElements.get('show-more-button')) {
+  customElements.define(
+    'show-more-button',
+    class ShowMoreButton extends HTMLElement {
+      constructor() {
+        super();
+        this.querySelector('button').addEventListener('click', (event) => {
+          this.expandShowMore(event);
+          const nextElementToFocus = event.target.closest('.parent-display').querySelector('.show-more-item');
+          if (nextElementToFocus && !nextElementToFocus.classList.contains('hidden') && nextElementToFocus.querySelector('input')) {
+            nextElementToFocus.querySelector('input').focus();
+          }
+        });
+      }
+      expandShowMore(event) {
+        const parentDisplay = event.target.closest('[id^="Show-More-"]').closest('.parent-display');
+        this.querySelectorAll('.label-text').forEach((element) => element.classList.toggle('hidden'));
+        parentDisplay.querySelectorAll('.show-more-item').forEach((item) => item.classList.toggle('hidden'));
+        if (!this.querySelector('.label-show-less')) {
+          this.classList.add('hidden');
+        }
+      }
+    }
+  );
+}
